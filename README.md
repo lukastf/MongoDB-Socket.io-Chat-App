b49087641aae5278f850a8a63f10a9513ad977d7
Eric Liu <eric__1234@outlook.com>
2017-09-03 16:51:19 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # MongoDB-Socket.io-Chat-App
 A simple Chat App built with MongoDB &amp; Socket.io.
+
+### Version
+1.0.0
+
+## Install Dependencies
+```bash
+npm install 
+```
+
+## Run Server
+```bash
+npm start
+```
+
+## Run App
+Open index.html
---
+++ server.js
@@ -0,0 +1,67 @@
+const mongo = require('mongodb').MongoClient;
+const client = require('socket.io').listen(4000).sockets;
+
+// Connects to Mongo
+var url = 'mongodb://localhost:27017/chatapp';
+mongo.connect(url, function(err, db) {
+	if (err) {
+		throw err;
+	}
+
+	console.log('MongoDB connected!');
+	//db.close();
+
+	// Connects to Socket.io
+	client.on('connection', function(socket) {
+		let chat = db.collection('chats');
+
+		// Creates function to send/emit status of messages
+		sendStatus = function(s) {
+			socket.emit('status', s); 
+		}
+
+		// Gets chats from mongo collection
+			//Limits to 100 chats only, then sorts them by id
+		chat.find().limit(100).sort({_id:1}).toArray(function(err, res) {
+			if (err) {
+				throw er;
+			}
+
+			// Emits the messages
+			socket.emit('output', res);
+		});
+
+		// Handles input events
+		socket.on('input', function(data) {
+			let name = data.name;
+			let message = data.message;
+
+			// Checks for the name and message
+			if (name == '' || message == '') {
+				// Sends error status
+				sendStatus('Please enter a valid name and message');
+			}
+			else {
+				chat.insert({name: name, message: message}, function() {
+					client.emit('output', [data]);
+
+					// Sends status object
+					sendStatus({
+						message: 'Message sent successfully',
+						clear: true
+					});
+				});
+			}
+		});
+
+		// Handles clearing function in index.html
+		socket.on('clear', function(data) { //Uses the clear variable declared above
+			// Removes all chats from collection
+			chat.remove({}, function() {
+				// Emits cleared
+				socket.emit('Successfully cleared');
+			});
+		});	
+	});
+});
+
